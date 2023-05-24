7c5071a8c126fc5d9f2c9a12fe684f5fb4104c0e
Sungchang Park <psch300@gmail.com>
2023-05-24 12:59:30 +0900

    feat(icons): add youtube, facebook icons

---
+++ packages/icons/src/Facebook.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+const SvgFacebook = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M17 2.119v3.168c-.301-.02-1.145-.05-2.108 0-1.205.063-1.386 1.026-1.386 1.491v2.486h3.373l-.451 3.541h-2.922V22H9.952v-9.195H7V9.264h2.952c-.01-.736-.024-2.293 0-2.64.03-.436-.12-1.989.994-3.2 1.114-1.212 1.897-1.305 2.952-1.398.843-.075 2.42.03 3.102.093Z"
+    />
+  </svg>
+);
+export default SvgFacebook;
---
+++ packages/icons/src/Youtube.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+const SvgYoutube = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M3 5a2 2 0 0 0-2 2v10.385a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H3Zm12.1 7.583a.5.5 0 0 0 0-.866l-4.275-2.468a.5.5 0 0 0-.75.433v4.936a.5.5 0 0 0 .75.433l4.275-2.468Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgYoutube;
