30ad15674cf42192713a6fc25a2dccf2ae4b2b4a
mym0404 <mym0404@gmail.com>
2023-06-19 08:33:38 +0900

    feat(timer): don't be shameful with your internals

--- packages/timer/src/index.ts
+++ packages/timer/src/index.ts
@@ -2,3 +2,5 @@ export * from './DueDateText';
 export * from './SecondsFormatter';
 export * from './useDueDateTicker';
 export * from './useReverseTicker';
+export * from './Ticker';
+export * from './useTicker';
