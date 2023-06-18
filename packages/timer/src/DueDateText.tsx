35dedffa6d6729e63108274c6f8e5ef2c91272aa
mym0404 <mym0404@gmail.com>
2023-06-19 08:12:55 +0900

    feat(timer): change DueDateText children seconds prop type & include isExpired option
    This is a brokne change

--- packages/timer/src/DueDateText.tsx
+++ packages/timer/src/DueDateText.tsx
@@ -4,14 +4,14 @@ import { useDueDateTicker, DueDateTickerProps } from './useDueDateTicker';
 
 type Props = {
   dueDate: string;
-  children: (text: string, remainSeconds: number) => ReactElement;
+  children: (text: string, meta: { remainSeconds: number; isExpired: boolean }) => ReactElement;
 } & DueDateTickerProps;
 export const DueDateText = ({ dueDate, children, ...options }: Props) => {
-  const { dueDateText, startTickerWithTargetDateTimeString, tickSec } = useDueDateTicker({
+  const { dueDateText, startTickerWithTargetDateTimeString, tickSec, isExpired } = useDueDateTicker({
     ...options,
   });
   useEffect(() => {
     startTickerWithTargetDateTimeString(dueDate);
   }, [dueDate, startTickerWithTargetDateTimeString]);
-  return children(dueDateText, tickSec);
+  return children(dueDateText, { isExpired, remainSeconds: tickSec });
 };
