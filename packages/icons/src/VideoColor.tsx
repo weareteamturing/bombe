b69f61e05296cd09a9007a2e5d86fe82e04f4ab6
Sungchang Park <psch300@gmail.com>
2023-06-19 15:38:35 +0900

    feat(icons): run svgr script to generate brand new icon files

--- packages/icons/src/VideoColor.tsx
+++ packages/icons/src/VideoColor.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { SVGProps } from 'react';
+import type { SVGProps } from 'react';
 const SvgVideoColor = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
     <path fill="#D9CDF9" d="M23 12c0 6.075-4.925 11-11 11S1 18.075 1 12 5.925 1 12 1s11 4.925 11 11Z" />
