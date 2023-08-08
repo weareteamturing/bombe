f62162794a1896cae4dcde54b311f9ca9cf321ef
mym0404 <mym0404@gmail.com>
2023-08-08 17:06:05 +0900

    feat(icons): add sound_on icon

---
+++ packages/icons/src/SoundOn.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgSoundOn = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M1 16.588V8.794c0-.43.349-.78.78-.78h3.184a.78.78 0 0 0 .55-.228l5.457-5.456a.78.78 0 0 1 1.33.552v19.302a.78.78 0 0 1-1.314.566l-5.475-5.17a.78.78 0 0 0-.535-.213H1.779a.78.78 0 0 1-.779-.78Z"
+    />
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M17.585 12.463a3.083 3.083 0 0 0-1.918-2.855.957.957 0 0 1 .724-1.771 4.997 4.997 0 0 1 0 9.251.956.956 0 1 1-.724-1.77 3.083 3.083 0 0 0 1.918-2.855Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M22.044 12.424a6.677 6.677 0 0 0-4.12-6.183.956.956 0 0 1 .728-1.769 8.59 8.59 0 0 1 5.305 7.952 8.59 8.59 0 0 1-5.305 7.953.956.956 0 1 1-.728-1.77 6.678 6.678 0 0 0 4.12-6.183Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgSoundOn;
