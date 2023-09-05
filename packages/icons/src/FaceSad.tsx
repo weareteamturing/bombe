e72d6290f1406beb0a6ade15462a0e73f0bccccc
zmin9 <o3o0903@gmail.com>
2023-09-05 12:47:00 +0900

    feat(icons): add face_happy, face_sad, new, my_bubble icon

---
+++ packages/icons/src/FaceSad.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgFaceSad = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M12 .98c-6.075 0-11 4.924-11 11 0 6.075 4.925 11 11 11s11-4.925 11-11c0-6.076-4.925-11-11-11ZM10.821 10c0 .939-.574 1.7-1.283 1.7s-1.284-.761-1.284-1.7.575-1.7 1.284-1.7c.709 0 1.283.761 1.283 1.7Zm4.85 0c0 .94-.575 1.7-1.284 1.7-.71 0-1.284-.76-1.284-1.7 0-.939.575-1.7 1.284-1.7.708 0 1.283.761 1.283 1.7Zm-7.08 4.985a.856.856 0 0 0 1.038 1.361 3.697 3.697 0 0 1 4.634-.026.856.856 0 1 0 1.102-1.309c-1.083-.912-3.944-2.182-6.773-.026Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgFaceSad;
