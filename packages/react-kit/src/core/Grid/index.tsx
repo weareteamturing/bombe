5fac34ded2232b1059935e9cc9559cef3ccec375
Sungchang Park <psch300@gmail.com>
2023-09-08 11:14:07 +0900

    feat(react-kit): create react-kit package

---
+++ packages/react-kit/src/core/Grid/index.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+
+const Grid = () => {};
+
+export default Grid;
---
+++ packages/react-kit/src/utils/styled-system/index.ts
@@ -0,0 +1,38 @@
+import css, { SystemStyleObject } from '@styled-system/css';
+import * as CSS from 'csstype';
+import { RequiredTheme, ResponsiveValue, system, Theme } from 'styled-system';
+
+// Support CSS custom properties in the `sx` prop
+type CSSCustomProperties = {
+  [key: `--${string}`]: string | number;
+};
+
+type CSSSelectorObject = {
+  [cssSelector: string]: SystemStyleObject | CSSCustomProperties;
+};
+
+type BetterSystemStyleObject = SystemStyleObject | CSSCustomProperties | CSSSelectorObject;
+
+export interface SxProp {
+  sx?: BetterSystemStyleObject;
+}
+
+const sx = (props: SxProp) => css(props.sx);
+
+type TextDecorationProps<ThemeType extends Theme = RequiredTheme> = {
+  textDecoration?: ResponsiveValue<CSS.Property.TextDecoration, ThemeType> | undefined;
+};
+const textDecoration = system({ textDecoration: { property: 'textDecoration', scale: 'textDecoration' } });
+
+type WhiteSpaceProps<ThemeType extends Theme = RequiredTheme> = {
+  whiteSpace?: ResponsiveValue<CSS.Property.WhiteSpace, ThemeType> | undefined;
+};
+const whiteSpace = system({ whiteSpace: { property: 'whiteSpace', scale: 'whiteSpace' } });
+
+type WordBreakProps<ThemeType extends Theme = RequiredTheme> = {
+  wordBreak?: ResponsiveValue<CSS.Property.WordBreak, ThemeType> | undefined;
+};
+const wordBreak = system({ wordBreak: { property: 'wordBreak', scale: 'wordBreak' } });
+
+export { sx, textDecoration, whiteSpace, wordBreak };
+export type { BetterSystemStyleObject, TextDecorationProps, WhiteSpaceProps, WordBreakProps };
