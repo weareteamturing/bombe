498f05406bdb1b52a77814291d0b7017c17a8636
Sungchang Park <psch300@gmail.com>
2023-09-12 18:41:42 +0900

    fix(react-kit): rollup fix

--- packages/react-kit/rollup.config.js
+++ packages/react-kit/rollup.config.js
@@ -2,6 +2,7 @@
 const babel = require('@rollup/plugin-babel');
 const commonjs = require('@rollup/plugin-commonjs');
 const nodeResolve = require('@rollup/plugin-node-resolve');
+const postcss = require('rollup-plugin-postcss');
 
 module.exports = {
   input: 'src/index.ts',
@@ -21,11 +22,11 @@ module.exports = {
   plugins: [
     nodeResolve({ extensions: ['.ts', '.tsx', '.js', '.jsx'] }),
     commonjs(),
+    postcss(),
     babel({
       extensions: ['.ts', '.tsx', '.js', '.jsx'],
       babelHelpers: 'bundled',
       rootMode: 'upward',
-      plugins: ['babel-plugin-styled-components'],
       presets: [
         [
           '@babel/preset-react',
