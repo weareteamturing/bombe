6cdca6a0c5aae155a3400c7d19fee6c40372930a
mym0404 <mym0404@gmail.com>
2023-09-18 11:26:26 +0900

    feat(icons): add logo icons

---
+++ packages/icons/src/LogoNaver.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgLogoNaver = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path fill="#fff" d="M14.865 4v8.073L9.163 4H3v16h6.135v-8.073L14.837 20H21V4h-6.135Z" />
+  </svg>
+);
+export default SvgLogoNaver;
