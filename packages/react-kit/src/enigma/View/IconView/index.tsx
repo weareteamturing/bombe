a897c4bef8983c279c615132b590c2013cbd02a5
Sungchang Park <psch300@gmail.com>
2023-10-18 11:37:31 +0900

    feat(react-kit): update enigma type with json schema generator

--- packages/react-kit/src/enigma/View/IconView/index.tsx
+++ packages/react-kit/src/enigma/View/IconView/index.tsx
@@ -1,14 +1,14 @@
 import * as icons from '@teamturing/icons';
 
 import StyledIcon from '../../../core/StyledIcon';
-import { IconView as IconViewType } from '../../types';
+import { IconViewType } from '../../types';
 
 type Props = {
   view: IconViewType;
 };
 
-const IconView = ({ view: { icon, iconProps = { color: 'icon/neutral', size: 24 } } }: Props) => (
-  <StyledIcon icon={icons[icon]} {...iconProps} />
+const IconView = ({ view: { icon, iconProps = { color: 'icon/neutral', size: 24 }, spaceProps } }: Props) => (
+  <StyledIcon icon={icons[icon]} {...iconProps} sx={{ ...spaceProps }} />
 );
 
 export default IconView;
