ca854b0f7110efa89afd94d8fdeebe646aca7c85
Sungchang Park <psch300@gmail.com>
2023-10-19 19:25:08 +0900

    fix(react-kit): update HorizontalDivider props

--- packages/react-kit/src/core/HorizontalDivider/index.tsx
+++ packages/react-kit/src/core/HorizontalDivider/index.tsx
@@ -2,17 +2,19 @@ import { ColorKey, SurfaceElevationKey } from '@teamturing/token-studio';
 import styled from 'styled-components';
 import { BorderColorProps, BorderProps, Theme, border } from 'styled-system';
 
+import { SxProp, sx } from '../../utils/styled-system';
+
 type Props = {
   width?: BorderProps['borderBottomWidth'];
   color?: BorderColorProps<Theme, ColorKey | SurfaceElevationKey>['borderBottomColor'];
   variant?: BorderProps['borderBottomStyle'];
-};
+} & SxProp;
 
-const HorizontalDivider = ({ width = 1, color = 'border/neutral', variant = 'solid' }: Props) => (
-  <BaseHorizontalDivider borderBottomWidth={width} borderBottomStyle={variant} borderBottomColor={color} />
+const HorizontalDivider = ({ width = 1, color = 'border/neutral', variant = 'solid', ...props }: Props) => (
+  <BaseHorizontalDivider borderBottomWidth={width} borderBottomStyle={variant} borderBottomColor={color} {...props} />
 );
 
-const BaseHorizontalDivider = styled.hr<BorderProps & BorderColorProps>`
+const BaseHorizontalDivider = styled.hr<BorderProps & BorderColorProps & SxProp>`
   display: block;
   margin: 0;
   padding: 0;
@@ -21,6 +23,7 @@ const BaseHorizontalDivider = styled.hr<BorderProps & BorderColorProps>`
   width: 100%;
 
   ${border}
+  ${sx}
 `;
 
 export default HorizontalDivider;
