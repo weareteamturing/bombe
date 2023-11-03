9b3a54a4626d7b1ab17c4e93d24e4b7c14e8f05f
Sungchang Park <psch300@gmail.com>
2023-11-03 12:30:15 +0900

    fix(react-kit): add Subheader to Datagrid with missing style

--- packages/react-kit/src/core/Datagrid/DatagridBody.tsx
+++ packages/react-kit/src/core/Datagrid/DatagridBody.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
+import { forcePixelValue } from '../../utils';
 import { SxProp, sx } from '../../utils/styled-system';
 
 type Props = {} & SxProp;
@@ -9,6 +10,12 @@ const DatagridBody = ({ ...props }: PropsWithChildren<Props>) => <BaseDatagridBo
 
 const BaseDatagridBody = styled.div<SxProp>`
   width: inherit;
+
+  & > div:not(:last-child) {
+    border-bottom-width: ${forcePixelValue(1)};
+    border-bottom-style: solid;
+    border-bottom-color: ${({ theme }) => theme.colors['border/neutral']};
+  }
 
   ${sx}
 `;
