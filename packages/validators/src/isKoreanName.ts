ba3ee6f9651606f2d9adb53dfa5477d5e921265f
Sungchang Park <psch300@gmail.com>
2023-11-29 18:07:56 +0900

    fix(validators): update isKoreanName

--- packages/validators/src/isKoreanName.ts
+++ packages/validators/src/isKoreanName.ts
@@ -1,12 +1,7 @@
-import { isKorean } from './isKorean';
-
 /**
  * 한국어 이름인지 판별합니다.
- * 1. 한글로 구성되어 있으며,
- * 2. 2-7자의 길이를 가져야합니다.
+ * 1. 자음, 모음, 공백, 특수문자를 제외한 완전한 한국어의 형태이며 2-7자의 길이를 가져야합니다.
  */
 export function isKoreanName(value: string): boolean {
-  const isValidLength = 2 <= value.length && value.length < 8;
-
-  return isKorean(value) && isValidLength;
+  return /^[\uac00-\ud7a3]{2,7}$/.test(value);
 }
