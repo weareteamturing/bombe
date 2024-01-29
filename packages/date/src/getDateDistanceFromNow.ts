f91a4951006621f05def5c50a3883f9423d7c99e
mym0404 <mym0404@gmail.com>
2024-01-29 16:42:58 +0900

    feat(date): enhance `getDateDistanceFromNow`

--- packages/date/src/getDateDistanceFromNow.ts
+++ packages/date/src/getDateDistanceFromNow.ts
@@ -1,8 +1,11 @@
 /**
  * 현재 시간과 주어진 시간과의 거리를 반환하는 함수입니다.
  */
-export function getDateDistanceFromNow(time?: string): string {
-  if (!(time && time.length === 0)) {
+export function getDateDistanceFromNow(
+  time?: string,
+  { includeYearPrefix }: { includeYearPrefix?: boolean } = { includeYearPrefix: false },
+): string {
+  if (!(time && time.length !== 0)) {
     return '';
   }
   const timeStamp = Date.parse(time);
@@ -16,20 +19,19 @@ export function getDateDistanceFromNow(time?: string): string {
   if (diffSec < hour) {
     return `${Math.floor(diffSec / min)}분 전`;
   }
-  if (diffSec < day) {
+  if (diffSec < hour * 24) {
     return `${Math.floor(diffSec / hour)}시간 전`;
   }
-  if (diffSec < month) {
-    return `${Math.floor(diffSec / day)}일 전`;
+  const date = new Date(time);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  if (includeYearPrefix) {
+    return `${year}년 ${month}월 ${day}일`;
+  } else {
+    return `${month}월 ${day}일`;
   }
-  if (diffSec < year) {
-    return `${Math.floor(diffSec / month)}개월 전`;
-  }
-  return `${Math.floor(diffSec / year)}년 전`;
 }
 
 const min = 60;
 const hour = min * 60;
-const day = hour * 24;
-const month = day * 30;
-const year = day * 365;
