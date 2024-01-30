7fa22d491d02ec26eaf206df2c210c622044dd34
mym0404 <mym0404@gmail.com>
2024-01-30 17:46:22 +0900

    feat(katex-utils): katex-util
    add markup transform logic + enhance formatkatextohtmlstring with performance optimization options

---
+++ packages/katex-utils/src/app-utils/excludeRootContentFrameClassCSSFromHtml.ts
@@ -0,0 +1,15 @@
+import { isEmptyString } from '@teamturing/validators';
+import { valid, parse } from 'node-html-parser';
+
+export function excludeRootContentFrameClassCSSFromHtml(html: string) {
+  try {
+    if (!html || isEmptyString(html) || !valid(html)) {
+      return html;
+    }
+    const root = parse(html);
+    (root.firstChild as unknown as HTMLElement)?.classList?.remove('_cms_content-frame');
+    return root.toString();
+  } catch (e) {
+    return html;
+  }
+}
