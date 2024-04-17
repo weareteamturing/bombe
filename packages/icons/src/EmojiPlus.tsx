2688fd8abbbdeae5f5a1d93c92a7d9507e3b8b37
Sungchang Park <psch300@gmail.com>
2024-04-17 12:51:41 +0900

    fix(icons): add EmojiPlus, EmojiPlusDash icons

---
+++ packages/icons/src/EmojiPlus.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgEmojiPlus = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M11.98 4.609a2.241 2.241 0 0 0 1.825 3.543h1.121v1.12a2.241 2.241 0 0 0 4.24 1.018c.303.875.468 1.815.468 2.793a8.517 8.517 0 1 1-7.653-8.474Zm2.443 6.77c0 .823-.504 1.49-1.125 1.49s-1.125-.667-1.125-1.49.504-1.49 1.125-1.49 1.125.667 1.125 1.49Zm-4.25 0c0 .823-.504 1.49-1.125 1.49s-1.125-.667-1.125-1.49.504-1.49 1.125-1.49 1.125.667 1.125 1.49Zm3.954 4.677a.75.75 0 0 0-.91-1.193 3.24 3.24 0 0 1-4.06.023.75.75 0 1 0-.967 1.147c.95.8 3.457 1.912 5.937.023Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M18.056 2.541a.941.941 0 1 0-1.882 0v2.481h-2.338a.913.913 0 1 0 0 1.825h2.338v2.367a.941.941 0 1 0 1.882 0V6.847h2.51a.912.912 0 1 0 0-1.825h-2.51v-2.48Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgEmojiPlus;
