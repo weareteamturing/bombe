b1fc1879ad6490e036e5221123a6c0290112b0ab
mym0404 <mym0404@gmail.com>
2024-05-09 14:54:07 +0900

    Revert "feat(icons): add icon"
    This reverts commit 4dc9a2ed9e26578d68e2617dab27f0cfb3b218f0.

--- packages/icons/src/PaperPenColor.tsx
+++ packages/icons/src/PaperPenColor.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgPaperPenColor = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 20 20" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
     <g clipPath="url(#paper_pen_color_svg__a)">
-      <rect width={13.714} height={16.831} x={2.917} y={1.371} fill="#C6D8FA" rx={1.175} />
+      <rect width={16.457} height={20.197} x={3.5} y={1.645} fill="#C6D8FA" rx={1.175} />
       <path
         fill="#6D99F1"
         fillRule="evenodd"
-        d="m16.005 9.908-3.533 3.533-.481 1.796-.317 1.184a.195.195 0 0 0 .239.24l1.182-.318 1.798-.482 3.532-3.532-2.42-2.42Zm3.881.41-1.87-1.87a.39.39 0 0 0-.55 0l-.965.964 2.42 2.421.965-.965a.388.388 0 0 0 0-.55Z"
+        d="m19.205 11.89-4.239 4.239-.578 2.156-.38 1.42a.235.235 0 0 0 .287.287l1.42-.38 2.156-.579 4.24-4.238-2.906-2.905Zm4.658.491-2.244-2.244a.466.466 0 0 0-.66 0L19.8 11.294l2.906 2.906 1.157-1.158a.469.469 0 0 0 0-.661Z"
         clipRule="evenodd"
       />
     </g>
     <defs>
       <clipPath id="paper_pen_color_svg__a">
-        <path fill="#fff" d="M0 0h20v20H0z" />
+        <path fill="#fff" d="M0 0h24v24H0z" />
       </clipPath>
     </defs>
   </svg>
