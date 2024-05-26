afd915d2c2c79bc5e5767919aa2d592772663718
zmin9 <o3o0903@gmail.com>
2024-05-27 08:53:10 +0900

    feat(icons): add home.svg

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -90,6 +90,7 @@ export { default as GunColorIcon } from './GunColor';
 export { default as HairIcon } from './Hair';
 export { default as HeadphoneIcon } from './Headphone';
 export { default as HeartIcon } from './Heart';
+export { default as HomeIcon } from './Home';
 export { default as InfiniteIcon } from './Infinite';
 export { default as InfoInCircleIcon } from './InfoInCircle';
 export { default as InstagramIcon } from './Instagram';
