29b9ae2b3d5aa4dde7d347e5d8b5cd49f78dadec
Mystic <mym0404@gmail.com>
2024-08-22 16:08:12 +0900

    fix(react-native-kit): eslint problem

--- packages/react-native-kit/example/metro.config.js
+++ packages/react-native-kit/example/metro.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 const { getConfig } = require('react-native-builder-bob/metro-config');
 
 const pkg = require('../package.json');
