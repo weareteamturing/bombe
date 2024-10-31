e4e7a80ef12a1f64a21f70091a04a34813957700
Sungchang Park <psch300@gmail.com>
2024-10-31 15:09:32 +0900

    feat(react-kit): add Drawer component

---
+++ packages/react-kit/src/core/Drawer/DrawerHeader.tsx
@@ -0,0 +1,31 @@
+import { PropsWithChildren } from 'react';
+
+import UnstyledDrawerHeader, { UnstyledDrawerHeaderProps } from './_UnstyledDrawerHeader';
+
+type Props = {} & UnstyledDrawerHeaderProps;
+
+const DrawerHeader = ({ children, sx, ...props }: PropsWithChildren<Props>) => (
+  <UnstyledDrawerHeader
+    {...props}
+    sx={{
+      pl: 5,
+      pr: 16,
+      py: 2,
+      minHeight: 56,
+      borderBottomWidth: 1,
+      borderBottomStyle: 'solid',
+      borderBottomColor: 'border/neutral',
+
+      display: 'flex',
+      flexDirection: 'column',
+      columnGap: 1,
+      flexWrap: 'nowrap',
+      ...sx,
+    }}
+  >
+    {children}
+  </UnstyledDrawerHeader>
+);
+
+export default DrawerHeader;
+export type { Props as DrawerHeaderProps };
