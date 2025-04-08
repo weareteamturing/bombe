72230ad7f13540292c49bbabed859accb2edb5b5
Sungchang Park <psch300@gmail.com>
2025-04-08 09:56:15 +0900

    feat(icons): update icon list

---
+++ packages/icons/src/AInCircleColor.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgAInCircleColor = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path fill="#E5E5E5" d="M0 0h24v24H0z" />
+    <path fill="#fff" d="M-121-426h1440v1826H-121z" />
+    <path
+      fill="#BACEF8"
+      fillRule="evenodd"
+      d="M1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12Zm8.556 4.746a1.178 1.178 0 1 1-2.243-.722l2.889-8.713a2.06 2.06 0 0 1 3.91.003l2.88 8.72a1.172 1.172 0 1 1-2.231.716l-.584-1.87h-4.04l-.581 1.866Zm2.563-8.218-1.395 4.466h2.867l-1.395-4.466h-.077Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#4880EE"
+      d="M8.431 17.573c.516 0 .971-.335 1.125-.827l.582-1.866h4.04l.583 1.87a1.172 1.172 0 1 0 2.23-.716l-2.879-8.72a2.06 2.06 0 0 0-3.91-.003l-2.889 8.713a1.178 1.178 0 0 0 1.118 1.549Zm2.293-4.58 1.395-4.465h.077l1.395 4.466h-2.867Z"
+    />
+  </svg>
+);
+export default SvgAInCircleColor;
