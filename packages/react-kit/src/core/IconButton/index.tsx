315cd9d46d4edf618a260506bfc1509c389d1af1
Sungchang Park <psch300@gmail.com>
2025-04-17 17:42:03 +0900

    refactor(react-kit): remove unused import

--- packages/react-kit/src/core/IconButton/index.tsx
+++ packages/react-kit/src/core/IconButton/index.tsx
@@ -2,7 +2,7 @@ import { ComponentType, Ref, SVGProps, forwardRef } from 'react';
 import styled from 'styled-components';
 import { ResponsiveValue, variant } from 'styled-system';
 
-import { BetterSystemStyleObject, sx } from '../../utils/styled-system';
+import { BetterSystemStyleObject } from '../../utils/styled-system';
 import Spinner from '../Spinner';
 import UnstyledButton, { UnstyledButtonProps } from '../_UnstyledButton';
 
