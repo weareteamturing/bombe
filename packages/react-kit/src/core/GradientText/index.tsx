6f3895fa69e91e9329e5752b6aea07e86240dbf8
Sungchang Park <psch300@gmail.com>
2025-07-28 14:51:05 +0900

    feat(react-kit): update GradientText component with additional variant

--- packages/react-kit/src/core/GradientText/index.tsx
+++ packages/react-kit/src/core/GradientText/index.tsx
@@ -1,16 +1,27 @@
 import styled from 'styled-components';
+import { variant } from 'styled-system';
 
+import { BetterSystemStyleObject } from '../../utils/styled-system';
 import Text from '../Text';
 import type { TextProps } from '../Text';
 
-type Props = {} & TextProps;
+type Props = { variant: 'violet' | 'pinkBlue' } & TextProps;
 
-const GradientText = styled(Text)<Props>`
-  background: ${({ theme }) => `linear-gradient(${theme.gradients['text/accent']})`};
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-`;
+const GradientText = styled(Text)(
+  {
+    backgroundClip: 'text',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+  },
+  ({ theme }) =>
+    variant<BetterSystemStyleObject>({
+      prop: 'variant',
+      variants: {
+        violet: { background: `linear-gradient(${theme.gradients['text/accent']})` },
+        pinkBlue: { background: `linear-gradient(${theme.gradients['text/accent/pinkblue']})` },
+      },
+    }),
+);
 
 export default GradientText;
 export type { Props as GradientTextProps };
