2d204311b83ce4fb2d0bb1fca56787002cd0ae8d
zmin9 <o3o0903@gmail.com>
2025-12-31 14:56:23 +0900

    feat(icons): add stop icon

---
+++ packages/icons/src/Icons/ic/Stop.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgStop = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M4 5.213C4 4.543 4.64 4 5.428 4h13.144C19.361 4 20 4.543 20 5.213v13.574c0 .67-.64 1.213-1.428 1.213H5.428C4.639 20 4 19.457 4 18.787V5.213Z"
+    />
+  </svg>
+);
+export default SvgStop;
