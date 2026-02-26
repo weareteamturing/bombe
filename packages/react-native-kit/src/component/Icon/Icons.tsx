aefb1da3dbdd2d8a4f4cfc6ce4de448b82070420
Sungchang Park <psch300@gmail.com>
2026-02-26 18:17:49 +0900

    feat(icons): add InkPenTwinkle icon

--- packages/react-native-kit/src/component/Icon/Icons.tsx
+++ packages/react-native-kit/src/component/Icon/Icons.tsx
@@ -124,6 +124,7 @@ export { default as heart } from '@teamturing/icons/svg/heart.svg';
 export { default as home } from '@teamturing/icons/svg/home.svg';
 export { default as infinite } from '@teamturing/icons/svg/infinite.svg';
 export { default as info_in_circle } from '@teamturing/icons/svg/info_in_circle.svg';
+export { default as ink_pen_twinkle } from '@teamturing/icons/svg/ink_pen_twinkle.svg';
 export { default as instagram } from '@teamturing/icons/svg/instagram.svg';
 export { default as kakao_color } from '@teamturing/icons/svg/kakao_color.svg';
 export { default as keyboard_down } from '@teamturing/icons/svg/keyboard_down.svg';
