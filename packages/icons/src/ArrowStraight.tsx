49abe3bf37024beeb246001518dee619c6177a49
zmin9 <o3o0903@gmail.com>
2026-03-12 18:50:52 +0900

    feat(icons): add arrows, isolate and typography icon

---
+++ packages/icons/src/ArrowStraight.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgArrowStraight = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="m15.772 2.503 4.17.32a1.502 1.502 0 0 1 1.38 1.381l.32 4.168a1 1 0 1 1-1.993.153l-.178-2.295-14.98 14.98c-.514.515-1.28.584-1.709.155-.43-.43-.36-1.195.154-1.71l14.98-14.982-2.297-.177a.999.999 0 0 1 .153-1.993Z"
+    />
+  </svg>
+);
+export default SvgArrowStraight;
